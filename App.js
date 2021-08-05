d1a2b56baf6130811c36d8315106272faefb4e45
Shinwoo Um <shinwooum@Shinwooui-MacBookPro.local>
2021-08-05 16:07:48 +0900

    asd

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
-import RootStack from './navigators/RootStack'
+//오세연 components
+import HomeScreen from './screens/HomeScreen';
+import MywordsScreen from './screens/MywordsScreen';
+import AfterSendingScreen from './screens/AfterSendingScreen';
+import VentingScreen from './screens/VentingScreen';
+import YourwordsScreen from './screens/YourwordsScreen';
+import ToSomeoneScreen from './screens/ToSomeoneScreen';
+import MyInfoScreen from './screens/MyInfoScreen';
 
-export default function  App(){
-  return <RootStack/>
+
+//엄신우 files
+import Main from './screens/Main';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Welcome from './screens/Welcome';
+import Popup from './screens/Popup';
+
+
+
+const Stack=createStackNavigator()
+
+// Home 위에 제목 styles 
+const HomeScreenOptions={
+  headerStyle:{backgroundColor:'lightblue'},
+  headerTitleStyle:{color:'black', justifyContent:'center'},
+  headerTintColor:'white',
 }
